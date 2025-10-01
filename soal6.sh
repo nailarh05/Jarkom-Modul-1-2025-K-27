52ba8821ea7caaa862e314b5c4271410fe72bda7
_ywizz <nailarh05@gmail.com>
2025-10-01 22:51:09 +0700

    Add files via upload

---
+++ soal6.sh
@@ -0,0 +1,6 @@
+wget --no-check-certificate -O "traffic.zip" "https://docs.google.com/uc?export=download&id=1bE3kF1Nclw0VyKq4bL2VtOOt53IC7lG5"
+unzip traffic.zip
+chmod +x traffic.sh
+./traffic.sh
+balik ke GNS3 - buka wireshrak pada node menwe
+ip.addr == 10.77.3.1 && dns
